b8e4a82d103c42c569c135d9b3a11afaa6e8c08b
KdntNinja <kaidentninja@gmail.com>
2023-12-04 21:18:22 +0000

    Update

--- main.py
+++ main.py
@@ -15,42 +15,49 @@ def home():
     return render_template("home.html")
 
 
+def get_db_connection():
+    con = sql.connect(DATABASE)
+    return con
+
+
 def execute_db_query(query, params):
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        cur.execute(query, params)
-        con.commit()
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute(query, params)
+    con.commit()
+    con.close()
 
 
 def create_table():
-    with sql.connect(DATABASE) as con:
-        cur = con.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS animals (
-                id INTEGER PRIMARY KEY,
-                animal_name TEXT NOT NULL,
-                animal_type TEXT NOT NULL,
-                animal_size TEXT NOT NULL,
-                animal_gender TEXT NOT NULL,
-                animal_breed TEXT NOT NULL,
-                animal_weight TEXT NOT NULL,
-                weight_unit TEXT NOT NULL,
-                animal_height TEXT NOT NULL,
-                height_unit TEXT NOT NULL,
-                animal_age TEXT NOT NULL,
-                animal_description TEXT NOT NULL,
-                animal_image TEXT NOT NULL
-            )
-        """)
-        con.commit()
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS animals (
+            id INTEGER PRIMARY KEY,
+            animal_name TEXT NOT NULL,
+            animal_type TEXT NOT NULL,
+            animal_size TEXT NOT NULL,
+            animal_gender TEXT NOT NULL,
+            animal_breed TEXT NOT NULL,
+            animal_weight TEXT NOT NULL,
+            weight_unit TEXT NOT NULL,
+            animal_height TEXT NOT NULL,
+            height_unit TEXT NOT NULL,
+            animal_age TEXT NOT NULL,
+            animal_description TEXT NOT NULL,
+            animal_image TEXT NOT NULL
+        )
+    """)
+    con.commit()
+    con.close()
 
 
 @app.route("/add-animal", methods=["GET", "POST"])
 def add_animal():
     if request.method == "POST":
-        msg = ""
         try:
             image = request.files["animal_image"]
+            filename = None
             if image.filename != "":
                 filename = secure_filename(image.filename)
                 image.save(os.path.join("data/img", filename))
@@ -72,7 +79,7 @@ def add_animal():
                 request.form["animal_description"],
                 animal_image
             )
-            query = "INSERT INTO animals (animal_name, animal_type, animal_size, animal_gender, animal_breed, animal_color, animal_weight, weight_unit, animal_height, height_unit, animal_age, animal_description, animal_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            query = "INSERT INTO animals (animal_name, animal_type, animal_size, animal_gender, animal_breed, animal_weight, weight_unit, animal_height, height_unit, animal_age, animal_description, animal_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
             execute_db_query(query, animal_data)
             msg = "Record successfully added"
         except sql.Error as e:
@@ -88,25 +95,48 @@ def loading():
     return render_template("loading.html")
 
 
-@app.route("/search")
+@app.route("/result-search")
 def search():
     query = request.args.get("q", "")
-    with sql.connect(DATABASE) as con:
-        con.row_factory = sql.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM animals WHERE animal_name LIKE ?", ("%" + query + "%",))
-        animals = cur.fetchall()
-    return render_template("result.html", animals=animals)
+    search_option = request.args.get("search_option", "animal_name")
+    con = get_db_connection()
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute(f"SELECT * FROM animals WHERE {search_option} LIKE ?", ("%" + query + "%",))
+    animals = cur.fetchall()
+    con.close()
+    return render_template("search_result.html", animals=animals)
+
+
+@app.route("/get-breeds")
+def get_breeds():
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("SELECT DISTINCT animal_breed FROM animals")
+    breeds = [row[0] for row in cur.fetchall()]
+    con.close()
+    return jsonify(breeds)
 
 
 @app.route("/view-animals")
 def view_animals():
-    with sql.connect(DATABASE) as con:
-        con.row_factory = sql.Row
-        cur = con.cursor()
-        cur.execute("SELECT * FROM animals")
-        rows = cur.fetchall()
+    con = get_db_connection()
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM animals")
+    rows = cur.fetchall()
+    con.close()
     return render_template("view_animals.html", rows=rows)
+
+
+@app.route("/delete-animal/<int:animal_id>", methods=["DELETE"])
+def delete_animal(animal_id):
+    con = get_db_connection()
+    cur = con.cursor()
+    cur.execute("DELETE FROM animals WHERE id = ?", (animal_id,))
+    con.commit()
+    con.close()
+    return '', 204
 
 
 if __name__ == "__main__":
